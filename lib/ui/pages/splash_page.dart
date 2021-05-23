e87bee1c0ef5379eb5081d9b961a138a7ec6b5b5
Saddam Azy <myxzlpltk@gmail.com>
2021-05-23 10:54:03 +0700

    splash page

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -6,13 +6,61 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Splash Page")),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Go To Sign In Page"),
-          onPressed: () {
-            BlocProvider.of<PageBloc>(context).add(GoToLoginPage());
-          },
+      backgroundColor: Colors.white,
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 136,
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage("assets/logo.png")),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 70, bottom: 16),
+              child: Text("New Experience",
+                  style: blackTextFont.copyWith(fontSize: 20)),
+            ),
+            Text(
+              "Watch a new movie much\neasier than any before",
+              style: greyTextFont.copyWith(
+                fontSize: 16,
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Container(
+              width: 250,
+              height: 46,
+              margin: EdgeInsets.only(top: 70, bottom: 19),
+              child: ElevatedButton(
+                child: Text(
+                  "Get Started",
+                  style: whiteTextFont.copyWith(fontSize: 16),
+                ),
+                style: ElevatedButton.styleFrom(primary: mainColor),
+                onPressed: () {},
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Already have an account? ",
+                  style: greyTextFont.copyWith(fontWeight: FontWeight.w400),
+                ),
+                GestureDetector(
+                  child: Text("Sign In", style: purpleTextFont),
+                  onTap: () {
+                    BlocProvider.of<PageBloc>(context).add(GoToLoginPage());
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
